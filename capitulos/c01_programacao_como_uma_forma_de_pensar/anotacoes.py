51ef70704b4f86ace42e5a2dbc7d9c7abc5bdc86
ale <alemoura.zyc@gmail.com>
2026-06-08 17:21:23 -0300

    feat: adicionar anotacoes sobre operadores aritmeticos

---
+++ capitulos/c01_programacao_como_uma_forma_de_pensar/anotacoes.py
@@ -0,0 +1,34 @@
+"""
+Capítulo 1: Programação como uma forma de pensar
+
+Tópicos estudados:
+- Operadores aritméticos
+- Expressões
+- Funções aritméticas
+- Strings
+- Valores e tipos
+- Linguagens formais e naturais
+- Depuração
+"""
+
+
+def operadores_aritmeticos():
+    """Aborda operadores aritméticos"""
+    print("--- Demonstração dos Operadores Aritméticos ---\n")
+    print("Adição (+): 30 + 12 =", 30 + 12)
+    print("Subtração (-): 43 - 1 =", 43 - 1)
+    print("Multiplicação (*): 6 * 7 =", 6 * 7)
+    print("Divisão (/): 84 / 2 =", 84 / 2)
+    # Nota de estudo: O resultado de (/) é 42.0 (float) em vez de 42 (int).
+    # Divisões comuns em Python sempre retornam um número de ponto flutuante.
+    # O operador (//) realiza a divisão inteira (floor division),
+    # que sempre arredonda o resultado para baixo, em direção ao "piso".
+    print("Divisão inteira (//): 85 // 2 =", 85 // 2)
+    print("Exponenciação (**) 7 ** 2 =", 7 ** 2)
+
+
+if __name__ == "__main__":
+    # Tire o '#' apenas da função que deseja testar
+
+    # operadores_aritmeticos()
+    pass
