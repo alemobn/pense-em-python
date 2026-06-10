414d0aa9b1378f3bf340fc1973d42774ac68e04c
ale <alemoura.zyc@gmail.com>
2026-06-10 15:43:53 -0300

    feat: adicionar anotacoes sobre funcoes aritmeticas

--- capitulos/c01_programacao_como_uma_forma_de_pensar/anotacoes.py
+++ capitulos/c01_programacao_como_uma_forma_de_pensar/anotacoes.py
@@ -38,9 +38,36 @@ def expressoes():
     # Toda expressão resulta em um Valor.
 
 
+def funcoes_aritmeticas():
+    """Aborda funções aritmeticas"""
+    print(
+        "--- Demonstrando algumas Funções Aritméticas "
+        "(Funções que operam com números) ---\n"
+    )
+    # Nota de estudo: O Python oferece diversas funções que operam com
+    # números. Por exemplo, a função "round" recebe um número do tipo float
+    # e o arredonda para o número inteiro mais próximo.
+    print("round(42.4) =", round(42.4))
+    print("round(42.6) =", round(42.6))
+    # A função "abs" calcula o valor absoluto de um número.
+    # Em caso de um número positivo, o valor absoluto é o próprio número.
+    print("abs(42) =", abs(42))
+    # Em caso de um número negativo, o valor absoluto é o valor positivo
+    # correspondente.
+    print("abs(-42) =", abs(-42))
+    # Quando utilizamos uma função dessa forma, dizemos que estamos
+    # "chamando" a função. Uma expressão que chama uma função é chamada
+    # de "chamada de função".
+    # Ao chamar uma função, é obrigatório utilizar parênteses. Caso você
+    # os omita, uma mensagem de erro será exibida.
+    # Para testar o erro, descomente a linha abaixo:
+    # print(abs 42) # SyntaxError: invalid syntax
+
+
 if __name__ == "__main__":
     # Tire o '#' apenas da função que deseja testar
 
     # operadores_aritmeticos()
     # expressoes()
+    # funcoes_aritmeticas()
     pass
