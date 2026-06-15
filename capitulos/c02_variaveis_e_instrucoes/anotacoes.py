412bed571ff49083ea969883db26df5f4c0d4653
ale <alemoura.zyc@gmail.com>
2026-06-15 16:50:23 -0300

    feat: iniciar arquivo de anotacoes referente ao capitulo 2

---
+++ capitulos/c02_variaveis_e_instrucoes/anotacoes.py
@@ -0,0 +1,21 @@
+"""
+Capítulo 2: Variáveis e instruções
+
+Tópicos estudados:
+- Variáveis
+- Diagramas de estado
+- Nomes de variáveis
+- Instrução import
+- Expressões e instruções
+- Função print
+- Argumentos
+- Comentários
+- Depuração
+- Glossário
+- Exercícios
+"""
+
+if __name__ == "__main__":
+    # Tire o '#' apenas da função que deseja testar
+
+    pass
