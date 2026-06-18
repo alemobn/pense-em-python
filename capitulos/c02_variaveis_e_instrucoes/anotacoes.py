41dbd747d2169d30989f9493caec9a2f25621053
ale <alemoura.zyc@gmail.com>
2026-06-18 14:42:30 -0300

    feat: adicionar anotacoes sobre variaveis

--- capitulos/c02_variaveis_e_instrucoes/anotacoes.py
+++ capitulos/c02_variaveis_e_instrucoes/anotacoes.py
@@ -15,7 +15,28 @@ Tópicos estudados:
 - Exercícios
 """
 
+
+def variaveis():
+    """
+    Demonstrar o conceito de atribuição e uso de variáveis na memória.
+    """
+    print("--- Demonstração de Variáveis ---\n")
+    # Uma "variável" é um nome que se refere a um valor.
+    n = 17
+    pi = 3.141592653589793
+    message = 'And now for something completely different'
+    # Podemos utilizar uma variável como parte de uma expressão
+    # com operadores aritméticos:
+    print(n + 25)
+    print(2 * pi)
+    # Também é possível empregar uma variável ao chamar uma
+    # função:
+    print(round(pi))
+    print(len(message))
+
+
 if __name__ == "__main__":
     # Tire o '#' apenas da função que deseja testar
 
+    # variaveis()
     pass
