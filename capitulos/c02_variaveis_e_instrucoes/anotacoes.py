cfbc583a86bffd24caf49605000e1146252d905d
ale <alemoura.zyc@gmail.com>
2026-06-19 15:56:19 -0300

    feat: adicionar anotacoes sobre nomes de variaveis

--- capitulos/c02_variaveis_e_instrucoes/anotacoes.py
+++ capitulos/c02_variaveis_e_instrucoes/anotacoes.py
@@ -53,9 +53,55 @@ def diagramas_de_estado():
     # e seus respectivos valores.
 
 
+def nomes_de_variaveis():
+    """
+    Explicar as regras de nomenclatura de variáveis e identificar palavras
+    reservadas.
+    """
+    print("--- Demonstração de Nomes de Variáveis ---\n")
+    # Nomes de variáveis não têm limite de tamanho.
+    # Podem conter tanto letras quanto números, mas não podem
+    # começar com um número.
+    # É possível utilizar letras maiúsculas, mas a recomendação
+    # é utilizar apenas letras minúsculas.
+    # O único sinal de pontuação permitido em nomes de variáveis
+    # é o "underscore(_)".
+    # -------------------------------------------------------------
+    # Se atribuirmos um nome inválido a uma variável, receberemos
+    # um erro de sintaxe. Por exemplo, o nome "million!" é inválido
+    # porque contém pontuação.
+    # Para testar o erro, descomente a linha abaixo:
+    # million! = 1000000
+    # -------------------------------------------------------------
+    # "76trombones = 'big parade'" é inválido porque começa com um
+    # número
+    # Para testar o erro, descomente a linha abaixo:
+    # 76trombones = 'big parade'
+    # -------------------------------------------------------------
+    # "class" também é inválido, mas pode não ser óbvio o porquê
+    # Para testar o erro, descomente a linha abaixo:
+    # class = 'Self-Defense Against Fresh Fruit'
+    # Acontece que class é uma "palavra reservada", ou seja, é
+    # uma palavra especial utilizada para especificar a estrutura
+    # de um programa. Palavras reservadas não podem ser utilizadas
+    # como nomes de variáveis.
+    # -------------------------------------------------------------
+    # Aqui está uma lista completa das palavras reservadas do
+    # Python (não é necessário memorizar essa lista):
+    palavras_reservadas = [
+        "False", "None", "True", "and", "as", "assert", "async", "await",
+        "break", "case", "class", "continue", "def", "del", "elif", "else",
+        "except", "finally", "for", "from", "global", "if", "import", "in",
+        "is", "lambda", "match", "nonlocal", "not", "or", "pass", "raise",
+        "return", "try", "while", "with", "yield"
+    ]
+    print(", ".join(palavras_reservadas))
+
+
 if __name__ == "__main__":
     # Tire o '#' apenas da função que deseja testar
 
     # variaveis()
     # diagramas_de_estado()
+    # nomes_de_variaveis()
     pass
