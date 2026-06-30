987565c5feb5a4457303b57377402e24ed83d629
ale <alemoura.zyc@gmail.com>
2026-06-30 17:29:19 -0300

    feat: adicionar resolucao do exercicio_2_1

---
+++ capitulos/c02_variaveis_e_instrucoes/exercicios.py
@@ -0,0 +1,23 @@
+# Exercícios do Capítulo 1 - Variáveis e instruções
+
+
+def exercicio_2_1():
+    """Exercício 1 (Capítulo 2)"""
+    print(
+        "Vimos que n = 17 é válido. Mas e 17 = n?"
+    )
+    print(
+        "\n"
+        "* R: 17 = n é inválido. No Python, a atribuição é feita da direita "
+        "para a esquerda, ou seja, o lado esquerdo do '=' deve ser sempre o "
+        "nome de uma variável (um identificador) e não um valor literal como "
+        "o número 17. O Python retornará um 'SyntaxError: cannot assign to "
+        "literal'."
+    )
+
+
+if __name__ == "__main__":
+    # Tire o '#' apenas do exercício que deseja testar
+
+    # exercicio_2_1()
+    pass
