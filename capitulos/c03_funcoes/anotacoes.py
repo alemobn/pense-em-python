d55d3b1bc538c286545e8bd7e5b37fd1db83697a
ale <alemoura.zyc@gmail.com>
2026-07-08 16:48:34 -0300

    feat: adicionar anotacoes sobre parametros

--- capitulos/c03_funcoes/anotacoes.py
+++ capitulos/c03_funcoes/anotacoes.py
@@ -46,8 +46,57 @@ def definindo_novas_funcoes():
     print_lyrics()  # Chamando a função (execução)
 
 
+def parametros():
+    """
+    Explica o conceito de parâmetros e argumentos, demonstrando como os valores
+    são atribuídos e copiados na memória durante a chamada de uma função.
+    """
+    print("--- Parâmetros e Argumentos ---\n")
+    print(
+        "Algumas das funções que vimos até agora exigem argumentos; "
+        "por exemplo, ao chamar 'abs', passamos um número como argumento. "
+        "Algumas funções exigem mais de um argumento: a função 'math.pow', "
+        "por exemplo, requer dois argumentos, a base e o expoente."
+        "\n"
+    )
+
+    # Definição de função que requer um argumento
+    def print_twice(string):  # (string) -> parâmetro da função
+        print(string)
+        print(string)
+
+    print(
+        "O nome da variável entre parênteses é chamado de 'parâmetro'. "
+        "Quando a função é chamada, o valor passado como argumento é "
+        "atribuído a esse parâmetro. Por exemplo, podemos chamar "
+        "'print_twice' da seguinte forma:\n"
+    )
+
+    # Atribuindo um valor (argumento) ao parâmetro da função
+    print_twice("Dennis Moore, ")
+    print()
+
+    print(
+        "Executar essa função tem o mesmo efeito que atribuir o argumento "
+        "ao parâmetro e, em seguida, executar o corpo da função, como no "
+        "exemplo a seguir:\n"
+    )
+
+    string = "Dennis Moore, "
+    print(string)
+    print(string)
+
+    print("\nTambém é possível utilizar uma variável como argumento:\n")
+    line = "Dennis Moore, "
+
+    # Utilizando uma variável para atribuir um valor ao parâmetro da função:
+    # (o valor de 'line' é atribuído ao parâmetro da função)
+    print_twice(line)
+
+
 if __name__ == "__main__":
     # Tire o '#' apenas da função que deseja testar
 
     # definindo_novas_funcoes()
+    # parametros()
     pass
