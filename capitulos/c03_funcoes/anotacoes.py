7bbaacaf2421ea836c0d72f97aa59a58d1103cb7
ale <alemoura.zyc@gmail.com>
2026-07-08 16:10:50 -0300

    feat: iniciar arquivo de anotacoes referente ao capitulo 3

---
+++ capitulos/c03_funcoes/anotacoes.py
@@ -0,0 +1,21 @@
+"""
+Capítulo 3: Funções
+
+Tópicos estudados:
+- Definindo novas funções
+- Parâmetros
+- Chamando funções
+- Repetição
+- Variáveis e parâmetros são locais
+- Diagrama de pilha
+- Tracebacks
+- Por que utilizar funções?
+- Depuração
+- Glossário
+- Exercícios
+"""
+
+if __name__ == "__main__":
+    # Tire o '#' apenas da função que deseja testar
+
+    pass
