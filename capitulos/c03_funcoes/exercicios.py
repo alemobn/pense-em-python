196aa643ac1478a709c3f7457e41ab8f8e0cb795
ale <alemoura.zyc@gmail.com>
2026-07-15 20:44:39 -0300

    feat: adicionar resolucao do exercicio 3_3

--- capitulos/c03_funcoes/exercicios.py
+++ capitulos/c03_funcoes/exercicios.py
@@ -40,9 +40,36 @@ def exercicio_3_2():
     repeat("string", 5)
 
 
+def exercicio_3_3():
+    """Exercício 3 (Capítulo 3)"""
+    print(
+        "Escolha qualquer outra função mencionada neste capítulo e solicite "
+        "ao assistente virtual que a escreva. O desafio é descrever a função "
+        "com precisão suficiente para que o assistente virtual entenda "
+        "exatamente o que você deseja. Utilize o vocabulário que você "
+        "aprendeu até agora neste livro.\n"
+    )
+    # Função escolhida: 'print_twice'
+    # Texto: Escreva uma função com o nome de 'print_twice', que recebe uma
+    # string como argumento, e exibe essa string duas vezes com uma quebra
+    # de linha.
+    # Resposta:
+
+    def print_twice(string):
+        """
+        Recebe uma string como argumento e a exibe duas vezes,
+        com uma quebra de linha entre elas.
+        """
+        print(string)
+        print(string)
+
+    print_twice("String.")
+
+
 if __name__ == "__main__":
     # Tire o '#' apenas do exercício que deseja testar
 
     # exercicio_3_1()
     # exercicio_3_2()
+    # exercicio_3_3()
     pass
