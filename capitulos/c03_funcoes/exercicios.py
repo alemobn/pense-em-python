7a0a43299cf7dbebf4043d3f6cc2bacb2bf70f39
ale <alemoura.zyc@gmail.com>
2026-07-15 19:19:10 -0300

    feat: adicionar resolucao do exercicio 3_2

--- capitulos/c03_funcoes/exercicios.py
+++ capitulos/c03_funcoes/exercicios.py
@@ -20,8 +20,29 @@ def exercicio_3_1():
     repeat("string", 5)
 
 
+def exercicio_3_2():
+    """Exercício 2 (Capítulo 3)"""
+    print(
+        "Se o resultado não incluir um loop 'for', pergunte: 'Poderia fazer "
+        "isso com um loop for?'.\n"
+    )
+
+    # Resposta:
+    def repeat(string, vezes):
+        """
+        Recebe uma string e um número inteiro, e imprime a string o número
+        de vezes especificado utilizando um loop for
+        """
+        for _ in range(vezes):
+            print(string, end="")
+        print()
+
+    repeat("string", 5)
+
+
 if __name__ == "__main__":
     # Tire o '#' apenas do exercício que deseja testar
 
     # exercicio_3_1()
+    # exercicio_3_2()
     pass
