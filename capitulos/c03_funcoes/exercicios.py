261c2140f1094a1e2590576b63d42e1d998bce18
ale <alemoura.zyc@gmail.com>
2026-07-15 19:12:58 -0300

    feat: adicionar resolucao do exercicio 3_1

--- capitulos/c03_funcoes/exercicios.py
+++ capitulos/c03_funcoes/exercicios.py
@@ -1,6 +1,27 @@
 # Exercícios do Capítulo 3 - Funções
 
+
+def exercicio_3_1():
+    """Exercício 1 (Capítulo 3)"""
+    print(
+        "Solicite ao seu assistente virtual favorite que 'escreva uma "
+        "função chamada 'repeat' que recebe uma string e um número inteiro, e "
+        "imprima a string o número de vezes especificado'.\n"
+    )
+
+    # Resposta:
+    def repeat(string, vezes):
+        """
+        Recebe uma string e um número inteiro, e imprime a string
+        o número de vezes especificado.
+        """
+        print(string * vezes)
+
+    repeat("string", 5)
+
+
 if __name__ == "__main__":
     # Tire o '#' apenas do exercício que deseja testar
 
+    # exercicio_3_1()
     pass
