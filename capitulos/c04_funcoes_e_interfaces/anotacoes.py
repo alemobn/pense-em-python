421901ff85820338185666c3da90fc957349bd3a
ale <alemoura.zyc@gmail.com>
2026-07-18 19:29:57 -0300

    feat: iniciar arquivo de anotacoes referente ao capitulo 4

---
+++ capitulos/c04_funcoes_e_interfaces/anotacoes.py
@@ -0,0 +1,21 @@
+"""
+Capítulo 4: Funções e interfaces
+
+Tópicos estudados:
+- Módulo jupyturtle
+- Desenhando um quadrado
+- Encapsulamento e generalização
+- Desenhando um círculo por aproximação
+- Refatorando o código
+- Diagrama de pilha
+- Docstrings
+- Depuração
+- Glossário
+- Exercícios
+"""
+
+
+if __name__ == "__main__":
+    # Tire o '#' apenas da função que deseja testar
+
+    pass
