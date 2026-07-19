e23b4995804f7062934c6eb3bff441007ef3ca09
ale <alemoura.zyc@gmail.com>
2026-07-19 18:58:38 -0300

    feat: adicionar anotacoes sobre modulo turtle

--- capitulos/c04_funcoes_e_interfaces/anotacoes.py
+++ capitulos/c04_funcoes_e_interfaces/anotacoes.py
@@ -15,7 +15,31 @@ Tópicos estudados:
 """
 
 
+def modulo_turtle():
+    """
+    Introdução aos gráficos de tartaruga
+    """
+    print("--- Gráficos com a Tartaruga ---\n")
+
+    # No livro, é utilizado o módulo 'jupyturtle'.
+    # Para utilizar o módulo nativo 'turtle' no Fedora, é necessário rodar:
+    # sudo dnf install python3-tkinter
+    from turtle import Turtle, done, forward, left
+
+    # Inicializa a tela gráfica e a tartaruga
+    Turtle()
+
+    # Desenha uma quina movendo-se para a frente e virando à esquerda
+    forward(50)
+    left(90)
+    forward(50)
+
+    # Mantém a janela gráfica aberta após a execução
+    done()
+
+
 if __name__ == "__main__":
     # Tire o '#' apenas da função que deseja testar
 
+    modulo_turtle()
     pass
