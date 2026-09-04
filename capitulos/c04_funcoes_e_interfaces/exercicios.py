adbca5cd1e3392faa82dc84bbae34a7aacb2800f
ale <alemoura.zyc@gmail.com>
2026-09-04 20:16:23 -0300

    feat: adicionar resolucao do exercicio 4_1

--- capitulos/c04_funcoes_e_interfaces/exercicios.py
+++ capitulos/c04_funcoes_e_interfaces/exercicios.py
@@ -1,6 +1,6 @@
 # Exercícios do Capítulo 4 - Funções e Interfaces
 
-from turtle import forward, pendown, penup
+from turtle import Turtle, forward, pendown, penup, left, done
 
 print(
     "Para esses exercícios, existem algumas outras funções da tartaruga "
@@ -24,8 +24,32 @@ def jump(length):
     pendown()
 
 
+def exercicio_4_1():
+    """Exercício 1 (Capítulo 4)"""
+    print(
+        "Escreva uma função chamada 'rectangle' que desenhe um retângulo "
+        "com os comprimentos dos lados fornecidos."
+    )
+    print()
+
+    def rectangle(width, height):
+        """
+        Desenha um retângulo com a largura (width) e altura (height) dadas.
+        """
+        for _ in range(2):
+            forward(width)
+            left(90)
+            forward(height)
+            left(90)
+
+    Turtle()
+    rectangle(100, 50)
+    done()
+
+
 if __name__ == "__main__":
     # Tire o '#' apenas do exercício que deseja testar
 
     # jump(100)
+    # exercicio_4_1()
     pass
